a1bb6fdcb41551908f527460b6c788d530d55142
Guillermo García Contreras <guillermogarciacontreras@Guillermos-iMac.local>
2021-06-16 11:23:38 -0500

    Added propTypes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,19 @@
 import 'styles/globals.css';
 
+import PropTypes from 'prop-types';
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
+
+MyApp.propTypes = {
+  Component: PropTypes.element,
+  pageProps: PropTypes.shape({}),
+};
+
+MyApp.defaultProps = {
+  Component: null,
+  pageProps: {},
+};
 
 export default MyApp;
