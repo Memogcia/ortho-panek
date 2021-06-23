04c43e51cce87c3095346d410b114e0a7cb42d39
Guillermo García <memogcia94@gmail.com>
2021-06-22 23:30:09 -0500

    getting todos

---
+++ src/pages/todos.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useQuery, gql } from '@apollo/client';
+
+const QUERY = gql`
+    query MyQuery {
+        todos {
+            title
+            user {
+                name
+            }
+        }
+    }
+`;
+
+function todos() {
+  const { data, loading, error } = useQuery(QUERY);
+  console.log('🚀 ~ file: todos.js ~ line 17 ~ todos ~ data', data);
+
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+
+  if (error) {
+    console.error(error);
+    return null;
+  }
+
+  return (
+    <div />
+  );
+}
+
+export default todos;
