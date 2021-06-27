f7f82e101f2939336208b3e34ad1e4fd9903955d
Guillermo García <v-guigarcia@expediagroup.com>
2021-06-26 22:25:44 -0500

    New provider

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,7 @@
 import "styles/globals.css";
 
 import PropTypes from "prop-types";
-import { UserProvider } from "@auth0/nextjs-auth0";
+import { Provider } from "next-auth/client";
 import Layout from "components/Layout";
 import { ThemeProvider, CssBaseline } from "@material-ui/core";
 import Head from "next/head";
@@ -11,7 +11,8 @@ import { useApollo } from "lib/apollo-client";
 import ClientOnly from "components/ClientOnly";
 
 function MyApp({ Component, pageProps }) {
-  const apolloClient = useApollo(pageProps.initialApolloState);
+  const token = pageProps?.session?.token;
+  const apolloClient = useApollo(token, pageProps.initialApolloState);
 
   if (typeof window === "undefined") {
     return (
@@ -22,7 +23,26 @@ function MyApp({ Component, pageProps }) {
   }
 
   return (
-    <UserProvider>
+    <Provider
+      // Provider options are not required but can be useful in situations where
+      // you have a short session maxAge time. Shown here with default values.
+      options={{
+        // Client Max Age controls how often the useSession in the client should
+        // contact the server to sync the session state. Value in seconds.
+        // e.g.
+        // * 0  - Disabled (always use cache value)
+        // * 60 - Sync session state with server if it's older than 60 seconds
+        clientMaxAge: 0,
+        // Keep Alive tells windows / tabs that are signed in to keep sending
+        // a keep alive request (which extends the current session expiry) to
+        // prevent sessions in open windows from expiring. Value in seconds.
+        //
+        // Note: If a session has expired when keep alive is triggered, all open
+        // windows / tabs will be updated to reflect the user is signed out.
+        keepAlive: 0,
+      }}
+      session={pageProps.session}
+    >
       <ApolloProvider client={apolloClient}>
         <ThemeProvider theme={theme}>
           <CssBaseline />
@@ -38,13 +58,13 @@ function MyApp({ Component, pageProps }) {
           </Layout>
         </ThemeProvider>
       </ApolloProvider>
-    </UserProvider>
+    </Provider>
   );
 }
 
 MyApp.propTypes = {
   Component: PropTypes.func,
-  pageProps: PropTypes.shape({ initialApolloState: {} }),
+  pageProps: PropTypes.shape({ initialApolloState: {}, session: {} }),
 };
 
 MyApp.defaultProps = {
