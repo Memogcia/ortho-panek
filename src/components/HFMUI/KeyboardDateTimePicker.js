a062294b2cb26b3d2da76a23ff621fff033e8edc
Guillermo García <v-guigarcia@expediagroup.com>
2021-06-28 18:36:13 -0500

    New component

---
+++ src/components/HFMUI/KeyboardDateTimePicker.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { Controller } from "react-hook-form";
+import PropTypes from "prop-types";
+import DateFnsUtils from "@date-io/date-fns";
+import {
+  KeyboardDateTimePicker,
+  MuiPickersUtilsProvider,
+} from "@material-ui/pickers";
+
+import { es } from "date-fns/locale";
+
+const DateTimePicker = ({ control, name, ...rest }) => {
+  return (
+    <MuiPickersUtilsProvider utils={DateFnsUtils} locale={es}>
+      <Controller
+        control={control}
+        name={name}
+        render={({ field }) => (
+          <KeyboardDateTimePicker
+            autoOk
+            fullWidth
+            disableFuture
+            disableToolbar
+            variant="inline"
+            // inputVariant="outlined"
+            format="dd/MM/yyyy"
+            ampm
+            invalidDateMessage="Fecha inválida"
+            invalidLabel="Fecha inválida"
+            {...field}
+            {...rest}
+          />
+        )}
+      />
+    </MuiPickersUtilsProvider>
+  );
+};
+
+DateTimePicker.propTypes = {
+  control: PropTypes.shape({}),
+  name: PropTypes.string,
+};
+
+DateTimePicker.defaultProps = {
+  control: {},
+  name: "",
+};
+
+export default DateTimePicker;
