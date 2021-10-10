e5db82bd49e530bdf0c86488d130caba4272c9e1
Guillermo García <memogcia94@gmail.com>
2021-10-09 21:35:13 -0500

    Added user Id as required

--- src/components/Calendar/schema.js
+++ src/components/Calendar/schema.js
@@ -1,6 +1,7 @@
 import * as Yup from "yup";
 
 export default Yup.object().shape({
+  user_id: Yup.string().required(),
   start_date_time: Yup.date(),
   end_date_time: Yup.date(),
   comments: Yup.string(),
