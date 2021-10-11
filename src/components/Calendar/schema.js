2db6979536ae87602921c5acb20302f5f900746e
Guillermo García <memogcia94@gmail.com>
2021-10-10 20:28:40 -0500

    Adding type to the validation schema

--- src/components/Calendar/schema.js
+++ src/components/Calendar/schema.js
@@ -4,5 +4,6 @@ export default Yup.object().shape({
   user_id: Yup.string().required(),
   start_date_time: Yup.date(),
   end_date_time: Yup.date(),
+  type: Yup.string().required(),
   comments: Yup.string(),
 });
