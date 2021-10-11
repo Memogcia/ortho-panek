94e554ccd9b00282a7f6e69a752c0f1ad637d4c1
Guillermo García <memogcia94@gmail.com>
2021-10-11 13:28:57 -0500

    Empty root

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,3 @@
-import { getSession, signIn } from "next-auth/client";
-
 export default function Home() {
   // if (isLoading) return <div>Loading...</div>;
   // if (error) return <div>{error.message}</div>;
@@ -8,23 +6,5 @@ export default function Home() {
   // return <div>TEST</div>;
   // }
 
-  return (
-    <button
-      type="button"
-      onClick={(e) => {
-        e.preventDefault();
-        signIn("auth0");
-      }}
-    >
-      Login
-    </button>
-  );
-}
-
-export async function getServerSideProps(context) {
-  return {
-    props: {
-      session: await getSession(context),
-    },
-  };
+  return null;
 }
