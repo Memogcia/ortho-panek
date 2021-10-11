1cc53478c6780d5e0b23b509cde6cb0bc71b1d0a
Guillermo García <memogcia94@gmail.com>
2021-10-10 20:28:05 -0500

    appointment types

---
+++ src/constants/appointmentsTypes.js
@@ -0,0 +1,15 @@
+// eslint-disable-next-line import/prefer-default-export
+export default [
+  "revisión",
+  "consulta",
+  "primera vez",
+  "estudio",
+  "inicio",
+  "presupuesto",
+  "retención",
+  "inconsistente",
+  "no pagó",
+  "recordar cita",
+  "paciente activo",
+  "cita extra",
+];
