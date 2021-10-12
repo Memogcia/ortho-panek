c4398a98747a6fa9980b133b34019275a24585c8
Guillermo García <memogcia94@gmail.com>
2021-10-12 01:48:21 -0500

    Providing custom authorization method

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -15,19 +15,6 @@ export default NextAuth({
   // https://next-auth.js.org/configuration/providers
   providers: [
     Providers.Credentials({
-      // The name to display on the sign in form (e.g. 'Sign in with...')
-      name: "Credentials",
-      // The credentials is used to generate a suitable form on the sign in page.
-      // You can specify whatever fields you are expecting to be submitted.
-      // e.g. domain, username, password, 2FA token, etc.
-      credentials: {
-        email: {
-          label: "Correo electrónico",
-          type: "text",
-          placeholder: "ejemplo@gmail.com",
-        },
-        password: { label: "Contraseña", type: "password" },
-      },
       async authorize(credentials, req) {
         let password;
 
