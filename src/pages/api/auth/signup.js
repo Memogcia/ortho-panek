0d4cf25ded6609523cb8f6b72ec926d27b52ee23
Guillermo García <memogcia94@gmail.com>
2021-10-12 01:49:00 -0500

    Signup working

--- src/pages/api/auth/signup.js
+++ src/pages/api/auth/signup.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcrypt";
+import { v1 as uuidv1 } from "uuid";
 
 async function handler(req, res) {
   // Only POST mothod is accepted
@@ -6,6 +7,7 @@ async function handler(req, res) {
     let response;
     let query;
     let variables;
+    let parsedResponse;
     // Getting email and password from body
     const { name, email, password } = req.body;
 
@@ -22,6 +24,7 @@ async function handler(req, res) {
         }
       }      
     `;
+
     variables = { email };
     response = await fetch(process.env.HASURA_GRAPHQL_API, {
       method: "POST",
@@ -31,21 +34,26 @@ async function handler(req, res) {
         variables,
       }),
     });
-    const parsedResponse = await response.json();
-    // Send error response if duplicate user is found
+    parsedResponse = await response.json();
 
     if ("data" in parsedResponse && parsedResponse.data.users.length > 0) {
       res.status(422).json({ message: "User already exists" });
     }
 
-    query = `query GetUserByEmailQuery($email: String = "") {
-        users(where: {email: {_eq: $email}}) {
-          id
-          name
+    query = `mutation InsertNewUser($email: String = "", $name: String = "", $password: String = "", $id: String = "") {
+        insert_users(objects: {name: $name, email: $email, password: $password, id: $id}) {
+          returning {
+            id
+          }
         }
-      }      
+      }                    
     `;
-    variables = { name, email, password: await bcrypt.hash(password, 12) };
+    variables = {
+      id: uuidv1(),
+      name,
+      email,
+      password: await bcrypt.hash(password, 12),
+    };
     response = await fetch(process.env.HASURA_GRAPHQL_API, {
       method: "POST",
       headers: { "x-hasura-admin-secret": process.env.HASURA_SECRET },
@@ -55,8 +63,11 @@ async function handler(req, res) {
       }),
     });
 
-    // Send success response
-    res.status(201).json({ message: "User created" });
+    parsedResponse = await response.json();
+
+    if ("errors" in parsedResponse)
+      res.status(500).json({ message: parsedResponse.errors[0].message });
+    else res.status(201).json({ message: "User created" });
   } else {
     // Response for other than POST method
     res.status(500).json({ message: "Route not valid" });
