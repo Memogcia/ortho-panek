c3aa85ad8b1f0c7fb38ea86ddd58b3e0ef8112e4
Guillermo García <memogcia94@gmail.com>
2021-10-12 01:27:26 -0500

    sign up

--- src/pages/auth/credentials-signup.js
+++ src/pages/auth/credentials-signup.js
@@ -2,21 +2,7 @@ import SignUp from "components/SignUp";
 import { getCsrfToken } from "next-auth/client";
 
 export default function credentialsSignIn({ csrfToken }) {
-  return (
-    <SignUp csrfToken={csrfToken} />
-    // <form method="post" action="/api/auth/callback/credentials">
-    //   <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
-    //   <label>
-    //     Username
-    //     <input name="username" type="text" />
-    //   </label>
-    //   <label>
-    //     Password
-    //     <input name="password" type="password" />
-    //   </label>
-    //   <button type="submit">Sign in</button>
-    // </form>
-  );
+  return <SignUp csrfToken={csrfToken} />;
 }
 
 export async function getServerSideProps(context) {
