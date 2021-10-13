93732106f0ac940c67216dce07fc1889ac8b501a
Guillermo García <memogcia94@gmail.com>
2021-10-13 00:16:55 -0500

    Cleaning code

--- src/pages/auth/credentials-signin.js
+++ src/pages/auth/credentials-signin.js
@@ -1,14 +1,5 @@
 import SignIn from "components/SignIn";
-import { getCsrfToken } from "next-auth/client";
 
-export default function credentialsSignIn({ csrfToken }) {
-  return <SignIn csrfToken={csrfToken} />;
-}
-
-export async function getServerSideProps(context) {
-  return {
-    props: {
-      csrfToken: await getCsrfToken(context),
-    },
-  };
+export default function credentialsSignIn() {
+  return <SignIn />;
 }
--- src/pages/auth/credentials-signup.js
+++ src/pages/auth/credentials-signup.js
@@ -1,14 +1,5 @@
 import SignUp from "components/SignUp";
-import { getCsrfToken } from "next-auth/client";
 
-export default function credentialsSignIn({ csrfToken }) {
-  return <SignUp csrfToken={csrfToken} />;
-}
-
-export async function getServerSideProps(context) {
-  return {
-    props: {
-      csrfToken: await getCsrfToken(context),
-    },
-  };
+export default function credentialsSignIn() {
+  return <SignUp />;
 }
