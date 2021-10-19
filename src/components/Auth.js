09fff41fec641487afe2caa9029ac4fdd7c3bda2
Guillermo García <memogcia94@gmail.com>
2021-10-19 01:06:58 -0500

    Adding a backdrop when loading user

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,9 +1,20 @@
-import React from "react";
+import { Backdrop, CircularProgress } from "@material-ui/core";
+
 import PropTypes from "prop-types";
-import { useSession } from "next-auth/client";
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import { useRouter } from "next/dist/client/router";
+import { useSession } from "next-auth/client";
+
+const useStyles = makeStyles((theme) => ({
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: "#fff",
+  },
+}));
 
 function Auth({ children }) {
+  const classes = useStyles();
   const [session, loading] = useSession();
   const router = useRouter();
   const isUser = !!session?.user;
@@ -19,11 +30,19 @@ function Auth({ children }) {
 
   // Session is being fetched, or no user.
   // If no user, useEffect() will redirect.
-  return <div>Loading...</div>;
+  return (
+    <Backdrop className={classes.backdrop} open>
+      <CircularProgress color="inherit" />
+    </Backdrop>
+  );
 }
 
 Auth.propTypes = {
   children: PropTypes.node,
+};
+
+Auth.defaultProps = {
+  children: null,
 };
 
 export default Auth;
