db037e3ad3d7d10a27b99c636442af12fefb6610
Guillermo García <memogcia94@gmail.com>
2021-10-19 01:22:33 -0500

    Updated copyright

--- src/components/Copyright.js
+++ src/components/Copyright.js
@@ -1,13 +1,13 @@
-import React from "react";
 import Link from "@material-ui/core/Link";
+import React from "react";
 import Typography from "@material-ui/core/Typography";
 
 export default function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {"Copyright © "}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
+      <Link color="inherit" href="https://ecgarcia.com/">
+        Ecgarcia
       </Link>{" "}
       {new Date().getFullYear()}.
     </Typography>
